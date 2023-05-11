bc374b9efee222248581ebfe4309f1f2943f51c2
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-11 20:55:27 +0600

    added to form in user

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css'
+
+
 function App() {
+  const haldleFormSubmet = (event) => {
+    event.preventDefault();
+    const form =event.target;
+    const name =form.name.value;
+    const email =form.email.value;
+    console.log(name,email);
+
+    const user ={name,email};
+    console.log(user);
+
+  }
 
   return (
     <>
-      <h1>Simple curd oparetion</h1>
+      <form onSubmit={haldleFormSubmet}>
+        <input type="text" name='name' placeholder='your name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='your email' />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
     </>
   )
 }
