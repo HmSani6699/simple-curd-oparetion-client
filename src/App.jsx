7ff340c171173173714786ed17026da3824f9180
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-12 09:56:48 +0600

    added to data fetch part

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,25 @@ import './App.css'
 function App() {
   const haldleFormSubmet = (event) => {
     event.preventDefault();
-    const form =event.target;
-    const name =form.name.value;
-    const email =form.email.value;
-    console.log(name,email);
-
-    const user ={name,email};
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
     console.log(user);
+
+    fetch('http://localhost:5000/user',{
+      method:'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+    })
+
+   
 
   }
 
